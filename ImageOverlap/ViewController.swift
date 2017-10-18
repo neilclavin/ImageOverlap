149c3e3eb2c1c38e0902f022479fc59ca5242ee5
Neil Clavin <neilclavin@de3mcl-505739.ber.global>
2017-10-18 15:09:18 +0200

    Alpha works!!! Hallelujah

--- ImageOverlap/ViewController.swift
+++ ImageOverlap/ViewController.swift
@@ -7,14 +7,61 @@
 //
 
 import UIKit
-import CoreImage
+import Foundation
+
+
 
 
 class ViewController: UIViewController {
+    
+
 
     @IBOutlet weak var image1: UIImageView!
-    
+
+
     @IBOutlet weak var image2: UIImageView!
+    
+    @IBAction func mergeImageTapped(_ sender: UIButton) {
+        
+        //var basicAlpha: Double = 0.5
+    
+        
+        let bottomImage = UIImage(named: "luca1")!
+        let bottomImageView = UIImageView(image: bottomImage)
+        bottomImageView.alpha = 0.5
+     
+        
+        let topImage = UIImage(named: "luca2")!
+        let topImageView = UIImageView(image: topImage)
+        topImageView.alpha = 0.5
+        
+        let newSize = CGSize(width:100, height: 100) // set this to what you need
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        
+        //let basicAlpha = CGFloat(0.5)
+        //let blendMode = 3
+        
+        bottomImage.draw(in: CGRect(origin: CGPoint.zero, size: newSize), blendMode: .normal, alpha: 1.0 )
+        
+       
+        
+        topImage.draw(in: CGRect(origin: CGPoint.zero, size: newSize), blendMode: .normal, alpha: 0.5 )
+        
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        mergedImage.image = newImage
+    }
+    
+    @IBOutlet weak var mergedImage: UIImageView!
+    
+    
+    
+    
+    
+    //func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
+    //    return CGRect(x: x, y: y, width: width, height: height)
+    //}
     
     
     
@@ -29,7 +76,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
